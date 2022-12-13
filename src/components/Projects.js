8a00739a4708a14e8c08daff91b35c77d0888f8a
Fiona Murray <fiona.c.murray@gmail.com>
2022-12-13 02:24:01 -0500

    projects are a carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,78 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import projects from './projectData'
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+
+import { Autoplay, Navigation, Pagination,A11y} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 
 export default function Projects() {
   return (
     <section id='projects'> 
      <div className='line'></div>
-    <div className="projects">
-    {projects.map(project => {
+     <Swiper
+       breakpoints={{
+        // when window width is >= 640px
+        640: {
+          width: 600,
+          slidesPerView: 1,
+        },
+        // when window width is >= 768px
+        768: {
+          width: 600,
+          slidesPerView: 1,
+        },
+      }}
+     modules={[Navigation,Pagination,Autoplay,A11y]}
+     spaceBetween={50}
+        loop={true}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        pagination={{clickable:true}}
+        navigation={true}
+        className="mySwiper"
+      >
+         {projects.map(project => {
       return (
-        <Card>
-        <Card.Body>
-          <Card.Title>{project.title}</Card.Title>
-          <Card.Text>{project.description}</Card.Text>
+        <SwiperSlide>
+      <Card className='proj'>
+        <Card.Body key={project.title}>
+            <h1>{project.title}</h1>
+         <p>{project.description}</p> 
+ 
           <div className="techs">
             {project.techs.map(tech  => {
               return (
-                <img src={tech[0]} alt={tech[1]} />
+                <img src={tech[0]} alt={tech[1]} key={tech[0]}/>
               )
             })}
           </div>
           {project.github &&
-              <Card.Link href={project.github}>
-              <Button variant="dark">Github</Button>
-              </Card.Link>
+              <a className='btn btn-dark' role='button' href={project.github}>
+              Github
+              </a>
            }
           {project.live &&
-              <Card.Link href={project.live}>
-              <Button variant="dark">Live</Button>
-              </Card.Link>
+              <a className='btn btn-dark' role='button' href={project.live}>
+              Live
+              </a>
            }
           {project.walkThrough &&
-              <Card.Link href={project.walkThrough}>
-              <Button variant="dark">Walk-through</Button>
-              </Card.Link>
-           }
-        </Card.Body>
-      </Card>
+              <a className='btn btn-dark' role='button' href={project.walkThrough}>
+              Walk-Through
+              </a>
+           } </Card.Body>   
+           </Card>   
+             </SwiperSlide>
       )
     })}
+      
+        </Swiper>
 
-    </div>
+
     </section>
   );
 }
